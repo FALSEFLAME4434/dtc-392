8d7bde0c5399156cd56265b1accde7407edff825
FALSEFLAME4434 <126906479+FALSEFLAME4434@users.noreply.github.com>
2023-03-03 09:53:49 -0800

    Initial files for MakeCode project

---
+++ images.g.ts
@@ -0,0 +1,44 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "image1":
+            case "Puddle":return img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . b b . . . . . 
+. . . . . . . . b 9 1 b . . . . 
+. . . b b b b b b 9 9 b . . . . 
+. . b 9 9 d 9 9 1 1 d b b b b . 
+. . . b d 9 9 9 1 1 9 9 d 9 1 b 
+. . b 9 d 9 9 9 9 9 9 9 d 9 9 b 
+. . b 9 3 3 9 9 9 9 9 d b b b . 
+. b 5 d 9 3 3 3 d d b b b b . . 
+b 5 5 5 b b b b b b b 9 9 1 b . 
+b 5 5 b . . . . . . b 3 9 9 b . 
+. b b . . . . . . . . b b b . . 
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+    helpers._registerFactory("song", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
